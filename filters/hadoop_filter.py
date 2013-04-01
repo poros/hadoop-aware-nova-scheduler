3c2374b985745443226da9a451e31e00c705097b
poros <antonio.uccio.verardi@gmail.com>
2013-04-02 01:24:25 +0200

    ApplicationAwareFilter Class methods changes + HadoopFilter Class implementation

--- filters/hadoop_filter.py
+++ filters/hadoop_filter.py
@@ -1,82 +1,123 @@
 from nova.scheduler import filters
 from nova.compute import api as compute
 
+# TODO: implement log operations
+
 
 class ApplicationAwareFilter(filters.BaseHostFilter):
     """Base class for all application aware filters. Retrieves instances information"""
+    # Inherit from the base filter class
 
+    # The application type is assumed to be saved in the instance metadata
     METADATA_APPLICATION_KEY = "application"
 
+    # Stores a reference to the compute API
     def __init__(self):
         self.compute_api = compute.API()
 
-    def _all_hosts(self, context, application):
+    def _all_hosts(self, context):
+        """Return a dictionary with all the host.
+            Key = instance_uuid, Value = host"""
+        all_hosts = {}
+        for instance in self.compute_api.get_all(context):
+            all_hosts[instance['uuid']] = instance['host']
+        return all_hosts
+
+    def _all_application_hosts(self, context, application):
         """Return a dictionary with all the host where an instance of the given application is running.
         Key = instance_uuid, Value = host"""
 
         all_hosts = {}
+        # Retrieves all the instances given a certain context
+        # TODO: Look if it returns deleted instances as well
         for instance in self.compute_api.get_all(context):
+            # Retrieves metadata of an instance
             metadata = self.compute_api.get_instance_metadata(context, instance)
+            # Looks for the application metadata
             if ApplicationAwareFilter.METADATA_APPLICATION_KEY in metadata:
+                # Stores the application metadata value
                 instance_application = metadata[ApplicationAwareFilter.METADATA_APPLICATION_KEY]
                 if instance_application == application:
+                    # Stores the entry (instance uuid, host)
                     all_hosts[instance['uuid']] = instance['host']
         return all_hosts
 
-    def _all_instances(self, context, application):
+    def _all_application_instances(self, context, application):
         """Return all the instances of a given application. All instances in the system if context is admin
         Key = instance_uuid, Value = instance"""
+
         all_instances = {}
+        # Retrieves all the instances given a certain context
+        # TODO: Look if it returns deleted instances as well
         for instance in self.compute_api.get_all(context):
+            # Retrieves metadata of an instance
             metadata = self.compute_api.get_instance_metadata(context, instance)
+            # Looks for the application metadata
             if ApplicationAwareFilter.METADATA_APPLICATION_KEY in metadata:
+                # Stores the application metadata value
                 instance_application = metadata[ApplicationAwareFilter.METADATA_APPLICATION_KEY]
                 if instance_application == application:
+                    # Stores the entry (instance uuid, instance reference)
                     all_instances[instance['uuid']] = instance
+        return all_instances
 
 
 class HadoopFilter(ApplicationAwareFilter):
     """Hadoop aware filter. Return the host where a DataNode is running"""
+    # Inherit from the base Application Aware Filter class
 
+    # Constant string values for Hadoop
     METADATA_APPLICATION_VALUE_HADOOP = "hadoop"
     METADATA_HADOOP_KEY = "hadoop"
     METADATA_HADOOP_VALUE_NAMENODE = "namenode"
-    METADATA_HADOOP_DATANODE_VALUE = "datanode"
-    METADATA_HADOOP_JOBTRACKER_VALUE = "jobtracker"
-    METADATA_HADOOP_TASKTRACKER_VALUE = "tasktracker"
+    METADATA_HADOOP_VALUE_DATANODE = "datanode"
+    METADATA_HADOOP_VALUE_JOBTRACKER = "jobtracker"
+    METADATA_HADOOP_VALUE_TASKTRACKER = "tasktracker"
 
+    # Method that every filter has to implement
+    # Return True if the host can be included in the candidates list, False otherwise
     def host_passes(self, host_state, filter_properties):
+        # Retrieves context from the the filter_properties part of the RPC message
+        # WARNING: Differs from what is written in the IBM paper
         context = filter_properties['context']
-        #scheduler_hints = filter_properties.get('scheduler_hints') or {}
-
+        # Retrieves image metadata from the request_spec.instance_properties part of the RPC message
+        # WARNING: Differs from what is written in the IBM paper
         spec = filter_properties.get('request_spec', {})
-        image_props = spec.get('image', {}).get('properties', {})
-        image_props = spec.get('instance_properties', {}).get('metadata', {})
-
-
-        me = host_state.host
-
-        affinity_uuids = scheduler_hints.get('different_host', [])
-        if isinstance(affinity_uuids, basestring):
-            affinity_uuids = [affinity_uuids]
-        if affinity_uuids:
-            all_hosts = self._all_hosts(context)
-            return any([i for i in affinity_uuids if all_hosts.get(i) == me])
-        # With no different_host key
+        image_metadata = spec.get('instance_properties', {}).get('metadata', {})
+        if image_metadata:
+            # Retrieves all Hadoop instances
+            # TODO: check if it already discriminate instances by user thanks to context
+            instances = self._all_application_instances(context, HadoopFilter.METADATA_APPLICATION_VALUE_HADOOP)
+            if instances:
+                # Selects the datanode instances within all the Hadoop instances
+                datanodes = [uiid for uiid in instances.keys()
+                    if self.get_instance_metadata(context, instances[uiid]).get[HadoopFilter.METADATA_HADOOP_KEY]
+                    == HadoopFilter.METADATA_HADOOP_VALUE_DATANODE]
+                if datanodes:
+                    # Retrieves all the hosts
+                    hosts = self._all_hosts(context)
+                    if hosts:
+                        # Get the current host
+                        me = host_state.host
+                        # Return if the current host match any of the hosts running a datanode application instance
+                        return any([uiid for uiid in datanodes if hosts.get(uiid) == me])
         return True
 
 
- #def get_all(self, context, search_opts=None, sort_key='created_at',
- #               sort_dir='desc', limit=None, marker=None):
-"""Get all instances filtered by one of the given parameters.
-
-If there is no filter and the context is an admin, it will retrieve
-all instances in the system.
-
-Deleted instances will be returned by default, unless there is a
-search option that says otherwise.
-
-The results will be returned sorted in the order specified by the
-'sort_dir' parameter using the key specified in the 'sort_key'
-parameter.
-"""
+# NOTES
+#scheduler_hints = filter_properties.get('scheduler_hints') or {}
+#image_props = spec.get('image', {}).get('properties', {})
+#def get_all(self, context, search_opts=None, sort_key='created_at',
+#               sort_dir='desc', limit=None, marker=None):
+#"""Get all instances filtered by one of the given parameters.
+#
+#If there is no filter and the context is an admin, it will retrieve
+#all instances in the system.
+#
+#Deleted instances will be returned by default, unless there is a
+#search option that says otherwise.
+#
+#The results will be returned sorted in the order specified by the
+#'sort_dir' parameter using the key specified in the 'sort_key'
+#parameter.
+#"""
